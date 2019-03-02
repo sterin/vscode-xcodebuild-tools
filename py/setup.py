d7df311bc8e7cff30e61f8858e3b1622dca5f009
Baruch Sterin <baruchs@gmail.com>
2019-03-01 20:46:57 -0800

    Python script: add dependencies to setup.py and update .gitignore for Python generated files

--- py/setup.py
+++ py/setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="xcodebuild_tools",
     py_modules=["xcodebuild_tools"],
+    install_requires=[
+        'click'
+    ],
     entry_points={
         'console_scripts': [
             'xcodebuild_tools = xcodebuild_tools:cli',
